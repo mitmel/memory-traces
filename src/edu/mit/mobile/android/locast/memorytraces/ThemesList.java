cfde132be6129e9c4d2951779b9f864cf872c9e7
Steve Pomeroy <steve@staticfree.info>
2012-08-01 14:26:44 -0400

    MemoryTraces: updates to use new sync framework

--- src/edu/mit/mobile/android/locast/memorytraces/ThemesList.java
+++ src/edu/mit/mobile/android/locast/memorytraces/ThemesList.java
@@ -2,7 +2,7 @@ package edu.mit.mobile.android.locast.memorytraces;
 
 import android.os.Bundle;
 import edu.mit.mobile.android.locast.data.Cast;
-import edu.mit.mobile.android.locast.sync.LocastSyncService;
+import edu.mit.mobile.android.locast.sync.LocastSync;
 import edu.mit.mobile.android.locast.tags.TagList;
 import edu.mit.mobile.android.locast.ver2.R;
 import edu.mit.mobile.android.widget.TextViewUtils;
@@ -14,7 +14,7 @@ public class ThemesList extends TagList {
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 
-		LocastSyncService.startExpeditedAutomaticSync(this, Cast.CONTENT_URI);
+		LocastSync.startExpeditedAutomaticSync(this, Cast.CONTENT_URI);
 
 		setTitle(getTitle());
 
